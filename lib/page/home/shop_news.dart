34497cd5a20eb0a2138a90769c336e9da7bda7fa
czq <790512128@qq.com>
2018-11-08 01:07:51 +0800

    跑马灯组件初步完成

--- lib/page/home/shop_news.dart
+++ lib/page/home/shop_news.dart
@@ -1,15 +1,67 @@
+import 'dart:async';
+
 import 'package:challenge_shop/data/viewModel/shop_banner_viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:marquee/marquee.dart';
 
-class ShopNews extends StatelessWidget {
+class ShopNews extends StatefulWidget {
+
   ShopBannerViewmodel shopBannerViewmodel;
 
   ShopNews(this.shopBannerViewmodel);
 
   @override
+  State<StatefulWidget> createState() {
+    return ShopNewsState();
+  }
+}
+
+class ShopNewsState extends State<ShopNews> {
+  ScrollController scroController;
+  Timer timer;
+  double position = 0.0;
+  double screenWidth;
+  double screenHeight;
+
+  @override
+  void initState() {
+    super.initState();
+    scroController = ScrollController();
+    timer = Timer.periodic(new Duration(milliseconds: 100), (timer) {
+      double maxScrollExtent = scroController.position.maxScrollExtent;
+      double pixels = scroController.position.pixels;
+      if (pixels + 3.0 >= maxScrollExtent) {
+        position = 0;
+        scroController.jumpTo(position);
+      }
+      position += 3.0;
+      scroController.animateTo(
+          position, duration: new Duration(milliseconds: 500),
+          curve: Curves.linear);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    screenWidth = MediaQuery
+        .of(context)
+        .size
+        .width;
+    screenHeight = MediaQuery
+        .of(context)
+        .size
+        .height;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (shopBannerViewmodel == null) {
+    if (widget.shopBannerViewmodel == null) {
       return Container();
     }
     return Container(
@@ -44,11 +96,19 @@ class ShopNews extends StatelessWidget {
                   height: 20,
                   width: 220,
                   margin: EdgeInsets.only(right: 11),
-                  child: Marquee(
-                      scrollAxis: Axis.vertical,
-                      velocity: 10,
-                      blankSpace: 5.0,
-                      child: getMarqueeContent()),
+                  child: ListView.builder(
+                      controller: scroController,
+                      itemCount:
+                      (widget.shopBannerViewmodel?.latestExchangeStatus
+                          ?.length ??
+                          0) > 0 ? double.maxFinite.toInt() : 0,
+                      itemBuilder: (context, index) {
+                        return getMarqueeContent(
+                            widget.shopBannerViewmodel
+                                ?.latestExchangeStatus[index %
+                                widget.shopBannerViewmodel
+                                    ?.latestExchangeStatus?.length]);
+                      }),
                 )
               ],
             ),
@@ -58,44 +118,46 @@ class ShopNews extends StatelessWidget {
       height: 50,
     );
   }
-}
 
-Widget getMarqueeContent() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.end,
-    crossAxisAlignment: CrossAxisAlignment.center,
-    children: <Widget>[
-      Expanded(
-        child: Text.rich(
-            TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                    text: "码农弟弟 兑换了 ",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Color(0xff717171),
-                    )),
-                TextSpan(
-                    text: '任天堂3ds任天堂3ds',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff717171),
-                      fontSize: 12,
-                    )),
-              ],
+
+  Widget getMarqueeContent(BannerNews banerNews) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Expanded(
+          child: Text.rich(
+              TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: "${banerNews.userName} 兑换了 ",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Color(0xff717171),
+                      )),
+                  TextSpan(
+                      text: '${banerNews.productName}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff717171),
+                        fontSize: 12,
+                      )),
+                ],
+              ),
+              overflow: TextOverflow.ellipsis),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 20),
+          child: Text(
+            "${banerNews.createTime}",
+            style: TextStyle(
+              fontSize: 12,
+              color: Color(0xff717171),
             ),
-            overflow: TextOverflow.ellipsis),
-      ),
-      Container(
-        margin: EdgeInsets.only(left: 20),
-        child: Text(
-          "12:26",
-          style: TextStyle(
-            fontSize: 12,
-            color: Color(0xff717171),
           ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
+
